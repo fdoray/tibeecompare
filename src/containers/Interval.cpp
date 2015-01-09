9d6f348fec478ae21b29e4b2d28258a46a2552d5
Francois Doray <fdoray@chromium.org>
2015-01-09 12:33:41 -0500

    Class: Interval.

---
+++ src/containers/Interval.cpp
@@ -0,0 +1,31 @@
+/* Copyright (c) 2014 Francois Doray <francois.pierre-doray@polymtl.ca>
+ *
+ * This file is part of tibeecompare.
+ *
+ * tibeecompare is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tibeecompare is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tibeecompare.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "containers/Interval.hpp"
+
+namespace tibee
+{
+namespace containers
+{
+
+Interval::Interval() : _low(0), _high(0) {}
+
+Interval::Interval(uint64_t low, uint64_t high)
+    : _low(low), _high(high) {}
+
+}  // namespace containers
+}  // namespace tibee
