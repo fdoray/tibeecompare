002b2a875857b2db99a93cb0beacfc4c5c393629
Francois Doray <fdoray@chromium.org>
2015-01-09 21:24:16 -0500

    Class: Punch block.

---
+++ src/execution_blocks/PunchBlock.cpp
@@ -0,0 +1,102 @@
+/* Copyright (c) 2014 Francois Doray <francois.pierre-doray@polymtl.ca>
+ *
+ * This file is part of tigerbeetle.
+ *
+ * tigerbeetle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tigerbeetle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tigerbeetle.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "execution_blocks/PunchBlock.hpp"
+
+#include "base/BindObject.hpp"
+#include "base/CompareConstants.hpp"
+#include "base/Constants.hpp"
+#include "base/print.hpp"
+#include "notification/Token.hpp"
+#include "value/MakeValue.hpp"
+
+namespace tibee {
+namespace execution_blocks {
+
+PunchBlock::PunchBlock()
+{
+}
+
+PunchBlock::~PunchBlock()
+{
+}
+
+void PunchBlock::Start(const value::Value* params)
+{
+    auto nameValue = params->GetField("name");
+    auto execValue = params->GetField("exec");
+    auto beginValue = params->GetField("begin");
+    auto endValue = params->GetField("end");
+    auto typeValue = params->GetField("type");
+
+    if (nameValue == nullptr || execValue == nullptr ||
+        beginValue == nullptr || endValue == nullptr ||
+        typeValue == nullptr)
+    {
+        base::tberror() << "Missing some parameters for punch block." << base::tbendl();
+        return;
+    }
+
+    _name = execValue->AsString();
+    _exec = execValue->AsString();
+    _beginEvent = beginValue->AsString();
+    _endEvent = endValue->AsString();
+    _isUST = typeValue->AsString() == "ust";
+}
+
+void PunchBlock::AddObservers(notification::NotificationCenter* notificationCenter)
+{
+    using notification::Token;
+
+    if (_isUST)
+    {
+        AddUstObserver(notificationCenter, Token(_beginEvent),
+                       base::BindObject(&PunchBlock::onBegin, this));
+        AddUstObserver(notificationCenter, Token(_endEvent),
+                       base::BindObject(&PunchBlock::onEnd, this));
+    }
+    else
+    {
+        AddKernelObserver(notificationCenter, Token(_beginEvent),
+                          base::BindObject(&PunchBlock::onBegin, this));
+        AddKernelObserver(notificationCenter, Token(_endEvent),
+                          base::BindObject(&PunchBlock::onEnd, this));
+    }
+}
+
+void PunchBlock::onBegin(const trace::EventValue& event)
+{
+    auto tid = ThreadForEvent(event);
+    if (!TidIsAnalyzed(tid))
+        return;
+}
+
+void PunchBlock::onEnd(const trace::EventValue& event)
+{
+    auto tid = ThreadForEvent(event);
+    if (!TidIsAnalyzed(tid))
+        return;
+}
+
+bool PunchBlock::TidIsAnalyzed(uint32_t tid) const
+{
+    auto thread_name = State()->CurrentNameForThread(tid);
+    return thread_name == _exec;
+}
+
+}  // namespace execution_blocks
+}  // namespace tibee
---
+++ src/execution_blocks/PunchBlock.hpp
@@ -0,0 +1,73 @@
+/* Copyright (c) 2014 Francois Doray <francois.pierre-doray@polymtl.ca>
+ *
+ * This file is part of tigerbeetle.
+ *
+ * tigerbeetle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tigerbeetle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tigerbeetle.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _TIBEE_BUILDERBLOCKS_PUNCHBLOCK_HPP
+#define _TIBEE_BUILDERBLOCKS_PUNCHBLOCK_HPP
+
+#include <string>
+
+#include "execution_blocks/AbstractExecutionBlock.hpp"
+#include "notification/NotificationCenter.hpp"
+#include "notification/Path.hpp"
+#include "quark/Quark.hpp"
+#include "trace/value/EventValue.hpp"
+#include "value/Value.hpp"
+
+namespace tibee {
+namespace execution_blocks {
+
+/**
+ * Block for tasks that are delimited by specified
+ * start and end events.
+ *
+ * @author Francois Doray
+ */
+class PunchBlock : public execution_blocks::AbstractExecutionBlock
+{
+public:
+    PunchBlock();
+    ~PunchBlock();
+
+    virtual void Start(const value::Value* params) override;
+    virtual void AddObservers(notification::NotificationCenter* notificationCenter) override;
+
+private:
+    void onBegin(const trace::EventValue& event);
+    void onEnd(const trace::EventValue& event);
+
+    bool TidIsAnalyzed(uint32_t tid) const;
+
+    // The name of the generated executions.
+    std::string _name;
+
+    // The name of the analyzed executable.
+    std::string _exec;
+
+    // The name of the begin event.
+    std::string _beginEvent;
+
+    // The name of the end event;
+    std::string _endEvent;
+
+    // Indicates whether the begin/end events are UST events.
+    bool _isUST;
+};
+
+}  // namespace execution_blocks
+}  // namespace tibee
+
+#endif // _TIBEE_BUILDERBLOCKS_PUNCHBLOCK_HPP
